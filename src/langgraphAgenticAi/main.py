68f8f5a382c19d951ea0fb4b9b7d95b4e429aa31
EngrHashim160 <engrhashim160@gmail.com>
2025-09-01 11:32:25 +0500

    worked in graph_builder, displayResult, and main.py updation

--- src/langgraphAgenticAi/main.py
+++ src/langgraphAgenticAi/main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from src.langgraphAgenticAi.ui.streamlitui.loadUI import LoadStreamlitUI
+from src.langgraphAgenticAi.LLMs.groqllm import GroqLLM
+from src.langgraphAgenticAi.graph.graph_builder import GraphBuilder
+from src.langgraphAgenticAi.ui.streamlitui.displayResult import DisplayResultStreamlit
 
 def load_langgraph_agenticai_app():
     """"
@@ -19,4 +22,28 @@ def load_langgraph_agenticai_app():
     user_message = st.chat_input("Enter your message: ")
     
     if user_message:
-        pass
+        try:
+            obj_llm_config = GroqLLM(user_controls_input=user_input)
+            model = obj_llm_config.get_llm_model()
+            
+            if not model:
+                st.error("Error: LLM Model could not be initialized.")
+                return
+            
+            # Initialize and set up the graph based on use case
+            usecase = user_input.get('selected_usecase')
+            
+            if not usecase:
+                st.error("Error: No use case selected.")
+                return 
+            
+            # Graph Builder
+            graph_builder = GraphBuilder(model)
+            try:
+                graph = graph_builder.setup_graph(usecase)
+                DisplayResultStreamlit(usecase, graph, user_message).display_result_on_ui()
+                
+            except Exception as e:
+                st.error(f"Error: Graph setup failed - {e}")
+        except Exception as e:
+            st.error(f"Error: Graph setup failed - {e}")
