11017d22cd48793dfcc2065c2020ce1a953196f1
EngrHashim160 <engrhashim160@gmail.com>
2025-09-01 15:16:58 +0500

    Test the project and resolve the bugs

--- src/langgraphAgenticAi/state/state.py
+++ src/langgraphAgenticAi/state/state.py
@@ -1,8 +1,8 @@
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated, List
 from langgraph.graph.message import add_messages
 
 class State(TypedDict):
     """
     Represent the structure of the state used in graph
     """
-    messages: Annotated[list, add_messages]
+    messages: Annotated[List, add_messages]
